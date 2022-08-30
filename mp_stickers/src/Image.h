685593dc7ff199d637aa488cdba543bf2b8b12f1
Kazmain Shah <kshah228@illinois.edu>
2022-08-30 10:37:50 -0500

    Started Image Class

--- mp_stickers/src/Image.h
+++ mp_stickers/src/Image.h
@@ -2,5 +2,31 @@
  * @file Image.h
  * Contains your declaration of the interface for the Image class.
  */
+#include <vector>
+
+class Image {
+  public:
+  Image();
+  void lighten();
+  void lighten(double amount);
+  void darken();
+  void darken(double amount);
+  void saturate();
+  void saturate(double amount);
+  void desaturate();
+  void desaturate(double amount);
+  void grayscale();
+  void rotateColor(double degrees);
+  void illinify();
+  void scale(double factor);
+  void scale(unsigned w, unsigned h);
+
+
+  private:
+  int width_;
+  int height_;
+  std::vector<std::vector<int>> pixels;
+
+};
 
 #pragma once
